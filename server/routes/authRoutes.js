f34158e0c184b41653e1280c338d3d341ec115c3
Joshua <jobisingh@algomau.ca>
2024-11-26 13:10:32 +0530

    Recitifed errors

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const { register, login } = require('../controllers/authController');
-const validateRegister = require('../middlewares/validateRegister'); // Optional validation middleware
+
 
 const router = express.Router();
 
 // User Registration
-router.post('/register', validateRegister, register);
+router.post('/register', register);
 
 // User Login
 router.post('/login', login);
